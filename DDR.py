5a53d13edbc4880b612b3e1d29e47d3eece8c8ea
Daniel-Raj <47554551+Daniel-Raj@users.noreply.github.com>
2020-02-19 11:07:34 +0530

    Update DDR.py

--- DDR.py
+++ DDR.py
@@ -1,19 +1,24 @@
-import urllib.request as fetchFrom
+import urllib.request as fetchFrom, os
 from bs4 import BeautifulSoup as include
 
 url = input("Enter the URL: ")
+if url=='':
+    print("Invalid URL...")
+else:
+    websiteResponse = fetchFrom.urlopen(url)
+    htmlCode = websiteResponse.read()
 
-websiteResponse = fetchFrom.urlopen(url)
-htmlCode = websiteResponse.read()
+    s = include(htmlCode,'html.parser')
+    #s.body.append(include('<script src="dani"></script>','html.parser'))
 
-s = include(htmlCode,'html.parser')
-s.body.append(include('<script src="dani"></script>','html.parser'))
-              
-htmlFile = open('../../../index.html','w',encoding='utf-8')
-htmlFile.write(str(s))
-htmlFile.close()
+    os.system('cmd /c "cordova create fname com.demo.ddr aname & cd fname & cordova platform add android"')
+    
+    htmlFile = open('fname\www\index.html','w',encoding='utf-8')
+    htmlFile.write(str(s))
+    htmlFile.close()
 
-"""import os
 
-os.system('cmd /k "cd/ & cd users\dani\desktop & rename index.html dani.html"')
-"""
+    os.system('cmd /c "cd fname & cordova build android"')
+    os.system('cmd /c "cd fname/platforms/android/app/build/outputs/apk/debug & start ."')
+
+
