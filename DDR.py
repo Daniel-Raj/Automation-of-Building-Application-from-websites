a31e233e1f681584f87b54ebfdf11257739853a5
Daniel-Raj <47554551+Daniel-Raj@users.noreply.github.com>
2020-03-12 10:50:24 +0530

    Update DDR.py

--- DDR.py
+++ DDR.py
@@ -1,24 +1,60 @@
+# Needed packages
+
+from tkinter import *
+from tkinter import ttk
+
 import urllib.request as fetchFrom, os
 from bs4 import BeautifulSoup as include
 
-url = input("Enter the URL: ")
-if url=='':
-    print("Invalid URL...")
-else:
-    websiteResponse = fetchFrom.urlopen(url)
-    htmlCode = websiteResponse.read()
+### Automation function Starts here
+def distatus():
+    try:
+        url = data.get()
+        appname = appdata.get()
+        
+        if url ==  '' and appname ==  '':
+            status1.set("Invalid URL...")
+        else:
+            
+            websiteResponse = fetchFrom.urlopen(url)
+            htmlCode = websiteResponse.read()
+            s = include(htmlCode,'html.parser')
+                  
+            os.system('cmd /c "cordova create fder com.demo.ddr ' + appname + ' & cd fder & cordova platform add android"')
+            status1.set('#1 - project created!')
+           
+            htmlFile = open('fder\www\index.html','w',encoding = 'utf-8')
+            htmlFile.write(str(s))
+            htmlFile.close()
+         
+            os.system('cmd /c "cd fder & cordova build android"')
+            status2.set('#2 - App created!')
+            os.system('cmd /c "cd fder/platforms/android/app/build/outputs/apk/debug & start ."')
+    except:
+        status1.set('#1 - Error!')
+        status2.set('#2 - Check the above fields or your internet connection')
+### Automation function ends here      
+        
+###GUI starts here
+window = Tk()
+window.title('Build ur Apk here! ')
+window.columnconfigure(0,weight = 1)
 
-    s = include(htmlCode,'html.parser')
-    #s.body.append(include('<script src="dani"></script>','html.parser'))
 
-    os.system('cmd /c "cordova create fname com.demo.ddr aname & cd fname & cordova platform add android"')
-    
-    htmlFile = open('fname\www\index.html','w',encoding='utf-8')
-    htmlFile.write(str(s))
-    htmlFile.close()
+status1 = StringVar()
+status2 = StringVar()
+data = StringVar()
+appdata = StringVar()
 
 
-    os.system('cmd /c "cd fname & cordova build android"')
-    os.system('cmd /c "cd fname/platforms/android/app/build/outputs/apk/debug & start ."')
+ttk.Label(window,text = 'URL').grid(sticky = 'w')
+ttk.Entry(window,textvariable = data,width = 50).grid(padx = 10)
+ttk.Label(window,text = 'App Name').grid(sticky = 'w')
+ttk.Entry(window,textvariable = appdata,width = 50).grid()
+ttk.Label(window,text = '').grid()
+ttk.Button(window,text = 'BUILD',command = distatus).grid()
+ttk.Label(window,text = '').grid()
+ttk.Label(window,textvariable = status1).grid(sticky = 'w')
+ttk.Label(window,textvariable = status2).grid(sticky = 'w')
 
-
+###GUI ends here
